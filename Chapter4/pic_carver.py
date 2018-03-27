507375bf64d1d55399f8be6b6ae738022878e1cb
Max MacCamy <maxmac12@gmail.com>
2018-03-26 20:23:15 -0700

    Cleanup

--- Chapter4/pic_carver.py
+++ Chapter4/pic_carver.py
@@ -35,56 +35,10 @@ def face_detect(path, file_name):
     return True
 
 
-def get_http_headers(http_payload):
-    """ Splits out the HTTP headers using regular expression from the given raw HTTP traffic"""
-    try:
-        # Split the headers off if it is HTTP traffic
-        headers_raw = http_payload[:http_payload.index(b"\r\n\r\n") + 2]
-
-        # Break out the headers
-        headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", headers_raw.decode('ascii')))
-    except:
-        return None
-
-    if "Content-Type" in headers:
-        return headers
-    else:
-        return None
-
-
-def extract_image(headers, http_payload):
-    """ Determines if an image has been received in an HTTP response based on the given response header"""
-    image      = None
-    image_type = None
-
-    try:
-        if "image" in headers['Content-Type']:
-            # Extract the image type from the MIME type.
-            image_type = headers['Content-Type'].split("/")[1]
-
-            # Extract the image body.
-            image = http_payload[http_payload.index(b"\r\n\r\n") + 8:]
-
-            # Decompress the image if compressed.
-            try:
-                if "Content-Encoding" in headers.keys():
-                    if headers['Content-Encoding'] == "gzip":
-                        image = zlib.decompress(image, 16 + zlib.MAX_WBITS)
-                    elif headers['Content-Encoding'] == "deflate":
-                        image = zlib.decompress(image)
-            except Exception as inst:
-                print(inst)
-                pass
-    except:
-        return None, None
-
-    return image, image_type
-
-
 def http_assembler(pcap_file):
-    carved_images  = 0
-    faces_detected = 0
-    img_data = []
+    num_images = 0
+    num_faces  = 0
+    img_data   = []
 
     # Read in the PCAP file for processing
     a = scapy.rdpcap(pcap_file)
@@ -93,61 +47,86 @@ def http_assembler(pcap_file):
     sessions = a.sessions()
 
     for session in sessions:
-        img_payload = b""
-        img_type    = b""
+        img_type     = b""
+        img_encoding = b""
+        img_payload  = b""
 
         for packet in sessions[session]:
             # Capture HTTP responses for possible image extraction.
             if packet.haslayer(http.HTTP):
                 pkt = packet.getlayer(http.HTTP)
 
-                # Skip HTTP request packets
                 if pkt.haslayer(http.HTTPRequest) or pkt.haslayer(http.HTTPResponse):
+
                     # Store any previous image data.
                     if img_type and img_payload:
-                        img_data.append((img_type, img_payload))
+                        img_data.append((img_type, img_encoding, img_payload))
 
-                    # Check if the response contains image data.
-                    header = dict(re.findall(b"(?P<name>.*?): (?P<value>.*?)\r\n", bytes(pkt)))
+                    # Reset the image type in preparation of new image data.
                     img_type = b""
 
+                    # Extract the HTTP header to determine image details
+                    header = dict(re.findall(b"(?P<name>.*?): (?P<value>.*?)\r\n", bytes(pkt)))
+
+                    # Attempt to recover image type and compression details from the HTTP header.
                     if b"Content-Type" in header:
                         if b"image" in header[b"Content-Type"]:
-                            # Get the new image type.
+                            # Extract the image type
                             img_type = header[b'Content-Type'].split(b"/")[1]
+
+                            # Extract the image encoding
+                            if b"Content-Encoding" in header:
+                                img_encoding = header[b'Content-Encoding']
+                            else:
+                                img_encoding = b""
 
                             # Reset the image data with the data contained in this response packet.
                             img_payload = bytes(pkt[scapy.Raw])
+                # Concatenate packets that contain additional image data.
                 elif img_type:
                     img_payload += bytes(pkt)
+        # Store any remaining image data from the current session.
         if img_type and img_payload:
-            img_data.append((img_type, img_payload))
+            img_data.append((img_type, img_encoding, img_payload))
 
-    for type, data in img_data:
-        # Store the image
-        file_name = "{}-pic_carver_{}.{}".format(pcap_file, carved_images, type.decode())
+    for (img_type, img_encoding, img_payload) in img_data:
 
-        with open("{}/{}".format(pictures_dir, file_name), "wb") as fd:
-            fd.write(data)
-            print("Saved: {}".format(file_name))
-
-            # Attempt face detection
+        # Decompress the image data if compressed.
+        if img_encoding:
             try:
-                result = face_detect("{}/{}".format(pictures_dir, file_name), file_name)
-
-                if result is True:
-                    faces_detected += 1
+                if img_encoding == b"gzip":
+                    img_payload = zlib.decompress(img_payload, 16 + zlib.MAX_WBITS)
+                elif img_encoding == b"deflate":
+                    img_payload = zlib.decompress(img_payload)
+                else:
+                    print("Unsupported image compression: {}".format(img_encoding.decode()))
             except Exception as inst:
                 print(inst)
                 pass
 
-        carved_images += 1
+        # Store the image
+        file_name = "{}-pic_carver_{}.{}".format(pcap_file, num_images, img_type.decode())
 
-    return carved_images, faces_detected
+        with open("{}/{}".format(pictures_dir, file_name), "wb") as fd:
+            fd.write(img_payload)
+
+            # Attempt face detection on image
+            try:
+                result = face_detect("{}/{}".format(pictures_dir, file_name), file_name)
+
+                if result is True:
+                    num_faces += 1
+            except Exception as inst:
+                print(inst)
+                pass
+
+            num_images += 1
+
+    return num_images, num_faces
 
 
 if __name__ == "__main__":
-    carved_images, faces_detected = http_assembler(pcap_file)
+    num_images, num_faces = http_assembler(pcap_file)
 
-    print("Extracted: {} images".format(carved_images))
-    print("Detected: {} faces".format(faces_detected))
+    print("Extracted: {} images".format(num_images))
+    print("Detected: {} faces".format(num_faces))
