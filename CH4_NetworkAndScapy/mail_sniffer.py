f93ca189bd1f99cce63d6c091324434bb18feb41
Max MacCamy <maxmac12@gmail.com>
2018-04-06 00:16:16 -0700

    Added scapy. to enumerations

--- CH4_NetworkAndScapy/mail_sniffer.py
+++ CH4_NetworkAndScapy/mail_sniffer.py
@@ -4,12 +4,12 @@ from scapy.all import *
 # Packet callback function
 def packet_callback(packet):
     print("a")
-    if packet[TCP].payload:
-        mail_packet = str(packet[TCP].payload)
+    if packet[scapy.TCP].payload:
+        mail_packet = str(packet[scapy.TCP].payload)
 
         if ("user" in mail_packet.lower()) or ("pass" in mail_packet.lower()):
-            print("[*] Server: {}".format(packet[IP].dst))
-            print("[*] {}".format(packet[TCP].payload))
+            print("[*] Server: {}".format(packet[scapy.IP].dst))
+            print("[*] {}".format(packet[scapy.TCP].payload))
 
 
 if __name__ == "__main__":
